3e3b80ee57fe8e5bb152f90193f87f1db0fe3fdf
Csillag61 <csillagok@gmail.com>
2019-02-26 19:42:15 -0500

    blue nav

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -11,7 +11,7 @@ class Contacts extends Component {
           return (
             <React.Fragment>
               <h1 className="display-4 mb-2">
-                <span className="text-primary">Contact</span> List
+                <span className="text-primary">Contact List</span>
               </h1>
               {contacts.map(contact => (
                 <Contact key={contact.id} contact={contact} />
